a93de7c6e0e3f6ff21462aca0ea37c4ad85e927f
raghu <iamraghu@live.com>
2023-03-16 02:44:05 -0400

    added second task

---
+++ com/rv/T1_2.java
@@ -0,0 +1,12 @@
+package com.rv;
+
+import java.util.Scanner;
+
+public class T1_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter something baba");
+        String userInput = scanner.next();
+        System.out.println("You entered, " + userInput);
+    }
+}
