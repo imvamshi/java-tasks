e7788d4f54c102c8f94ac4bf62d7bfd505b77977
raghu <iamraghu@live.com>
2023-03-16 00:36:21 -0400

    first task commit

---
+++ com/rv/T1_1.java
@@ -0,0 +1,16 @@
+package com.rv;
+
+public class T1_1 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 4;
+
+        System.out.println(a + ", " + Integer.toString(b));
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + ", " + Integer.toString(b));
+    }
+}
