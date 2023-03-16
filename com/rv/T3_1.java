09cf3203b8705d4421431e54db73f832300d287f
raghu <iamraghu@live.com>
2023-03-16 12:39:18 -0400

    added task 3

---
+++ com/rv/T3_1.java
@@ -0,0 +1,10 @@
+package com.rv;
+
+public class T3_1 {
+    public static void main(String[] args) {
+        int no = 3;
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(Integer.toString(no) + " X " + Integer.toString(i) + " = " + Integer.toString(no * i));
+        }
+    }
+}
