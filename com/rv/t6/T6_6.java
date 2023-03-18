9e8ebc23f0c44656546dae46e116e3bfb9e5d9de
raghu <iamraghu@live.com>
2023-03-18 15:39:57 -0400

    simulated object constructor overloading

--- com/rv/t6/T6_6.java
+++ com/rv/t6/T6_6.java
@@ -1,2 +1,20 @@
-package com.rv.t6;public class T6_6 {
+package com.rv.t6;
+
+public class T6_6 {
+    public static void main(String[] args) {
+        Programming obj1 = new Programming(); // prints "I love programming languages"
+        Programming obj2 = new Programming("Java"); // prints "I love Java"
+        Programming obj3 = new Programming("Python"); // prints "I love Python"
+    }
+}
+
+class Programming {
+
+    public Programming() {
+        System.out.println("I love programming languages");
+    }
+
+    public Programming(String language) {
+        System.out.println("I love " + language);
+    }
 }
